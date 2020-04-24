5d8f6ddcd2b7e20e080a73dc5cd9df7b059d8d9a
norika <veronica.shavrova@gmail.com>
2020-04-24 17:28:47 +0300

    added getUser method

--- src/main/java/com/tms/api/users/controller/UserController.java
+++ src/main/java/com/tms/api/users/controller/UserController.java
@@ -9,12 +9,12 @@ import com.tms.api.users.model.user.UserResponseModel;
 import com.tms.api.users.service.UserService;
 import com.tms.api.users.util.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,23 +29,23 @@ public class UserController {
     @Autowired
     UserMapper mapper;
 
+    @Autowired
+    private Environment env;
 
 
     @GetMapping("/status")
-    public String status(){
-        return "Working!";
+    public String status() {
+        return "Secret: " + env.getProperty("jwt.secret") + "\ntest.prop = " + env.getProperty("test.prop");
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    //@PreAuthorize("hasAuthority('ROLE_ADMIN') or hasAuthority('ROLE_USER')")
     public ResponseEntity<UserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel createdUser) {
         UserDto dto = service.createUser(mapper.createRequestToDto(createdUser));//map request to dto
         return ResponseEntity.status(HttpStatus.CREATED).body(mapper.createResponseFromDto(dto));
     }
 
     @GetMapping
-   // @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     ResponseEntity<Page<UserResponseModel>> getAllUsers(Pageable page) {
         Page<UserDto> userDtos = service.findPage(page);
         return ResponseEntity.ok(userDtos.map(dto -> mapper.createResponseFromDto(dto)));
@@ -53,16 +53,19 @@ public class UserController {
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    //@PreAuthorize("hasAuthority('ROLE_ADMIN') or hasAuthority('ROLE_USER')")
-    public ResponseEntity<UserResponseModel> updateUser(@Valid @RequestBody UpdateUserRequestModel userRequestModel){
+    public ResponseEntity<UserResponseModel> updateUser(@Valid @RequestBody UpdateUserRequestModel userRequestModel) {
         UserDto dto = service.updateUser(mapper.updateRequestToDto(userRequestModel));
         return ResponseEntity.ok(mapper.createResponseFromDto(dto));
     }
 
     @DeleteMapping("/{id}")
-  //  @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public ResponseEntity<Void> deleteUser(@PathVariable("id") String id){
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") String id) {
         service.deleteUser(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseModel> getUser(@PathVariable("id") String id) {
+        return ResponseEntity.ok(mapper.createResponseFromDto(service.getUser(id)));
     }
 }
